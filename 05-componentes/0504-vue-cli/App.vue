c6976694bfaff4cfef7d148d7a1c04afe45a7b8b
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2021-07-14 11:55:03 -0300

    Aula 5 Vue

---
+++ 05-componentes/0504-vue-cli/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>{{ mensagem }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            mensagem: "Verificando se funciona"
+        };
+    }
+};
+</script>
+
+<style>
+</style>
