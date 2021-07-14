812d9fa8b88b1fc7fe4e7c5f14d65ac57fc23030
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2021-07-14 15:23:02 -0300

    Aula 6 Vue

---
+++ 06-animacoes/0601-transition/exercicio.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="modal =!modal">Ativar</button>
+    <transition name="testetransition">
+      <div v-show="modal">Teste</div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      modal: false
+    };
+  }
+}
+</script>
+
+<style>
+.testetransition-enter, .testetransition-leave-to {
+  opacity: 0;
+}
+.testetransition-enter-active, .testetransition-leave-to {
+  transition: opacity 0.3s;
+}
+</style>
