6b75808a8a094b588b16074a360123ae2b0ba9cb
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2021-07-16 15:02:56 -0300

    Aula 9.12 Vue

--- ranek/src/router/index.js
+++ ranek/src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Produto from '../views/Produto.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
-
+  {
+    path: '/produto/:id',
+    name: 'produto',
+    component: Produto,
+    props: true
+  }
 ]
 
 const router = new VueRouter({
