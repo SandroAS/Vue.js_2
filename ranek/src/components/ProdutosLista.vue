0485038bbc65531e48a5981ddaec702019d2ab70
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2021-07-15 21:22:08 -0300

    Aula 9.5 Vue

---
+++ ranek/src/components/ProdutosLista.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" alt="produto.fotos[0].titulo">
+      <p class="titulo">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p class="titulo">{{ produto.descricao }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+import { api } from "@/services.js";
+
+export default {
+  data() {
+    return {
+        produtos: null
+    }
+  },
+  methods: {
+    getProdutos() {
+
+      api.get("/produto")
+      .then(response => {
+        this.produtos = response.data;
+      });
+
+      // fetch("http://localhost:3000/produto")
+      // .then(response => response.json())
+      // .then(response => {
+      //   this.produtos = response;
+      // });
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
+}
+</script>
+
+<style>
+
+</style>
